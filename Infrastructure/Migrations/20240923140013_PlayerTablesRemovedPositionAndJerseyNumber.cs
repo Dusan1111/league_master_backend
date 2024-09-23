17a19b630fcf17c6235eabfe5b519a5ecd207da9
Dusan1111 <80171059+Dusan1111@users.noreply.github.com>
2024-09-23 16:03:10 +0200

    Migration for removing JerseyNumber and Position fields from tblPlayer.

---
+++ Infrastructure/Migrations/20240923140013_PlayerTablesRemovedPositionAndJerseyNumber.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerTablesRemovedPositionAndJerseyNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JerseyNumber",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Players");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JerseyNumber",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
